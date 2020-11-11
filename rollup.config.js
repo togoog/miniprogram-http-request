8d2cf6d0bc7f9ac2b6e06e3c8647eb17dba7bb8a
sanwi <1304082170@qq.com>
2020-11-11 19:22:01 +0800

    config:rollup config

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import resolve from 'rollup-plugin-node-resolve'
+import json from 'rollup-plugin-json'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+
+export default [
+    {
+        input: 'src/index.js',
+        output: {
+            file: 'dist/miniprogram-http-request.js'
+        },
+        plugins: [
+            resolve(),
+            json(),
+            commonjs(),
+            babel({
+                presets: [
+                    ["@babel/preset-env"]
+                ],
+                exclude: 'node_modules/**'
+            }),
+        ]
+    },
+    {
+        input: 'src/index.js',
+        output: {
+            file: 'dist/miniprogram-http-request.main.js'
+        },
+        plugins: [
+            resolve(),
+            json(),
+            commonjs(),
+            babel({
+                presets: [
+                    ["@babel/preset-env"]
+                ],
+                exclude: 'node_modules/**'
+            }),
+            terser()
+        ]
+    }
+]

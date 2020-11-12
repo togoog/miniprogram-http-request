f0626d46f215ebaf6b822b5cba5966d574590e04
san-wi <1304082170@qq.com>
2020-11-12 10:33:39 +0800

    feat(helper):parse URL params

---
+++ src/helpers/parseURLParams.js
@@ -0,0 +1,23 @@
+function paramsSerializer(value) {
+  return JSON.parse('{' + value.replace(/=/g, ':').replace(/&/g, ',') + '}')
+}
+
+function parseURLParams(url) {
+  let index = url.indexOf('?');
+  let copy = url;
+
+  let params;
+  //url has params
+  if(index > -1){
+    url = url.substring(0, index);
+    copy = copy.replace(url + '?', '');
+    params = paramsSerializer(copy);
+  }
+
+  return {
+    url,
+    params
+  }
+}
+
+export default parseURLParams
